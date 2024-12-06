858c12d472fa0587109dd8f6c34ecf748f35fe23
Jason Richard Kendrick <jasonrichardkendrick@gmail.com>
2024-12-06 22:42:58 +0530

    modified auth attributes

--- src/server/routes/auth/login.ts
+++ src/server/routes/auth/login.ts
@@ -1,34 +1,32 @@
-import { OpenAPIHono, createRoute } from '@hono/zod-openapi'
-import { setCookie } from 'hono/cookie'
+import { OpenAPIHono, createRoute } from "@hono/zod-openapi";
+import { setCookie } from "hono/cookie";
 
-import { users } from '@/db/schema'
-import { and, eq } from 'drizzle-orm'
+import { users } from "@/db/schema";
+import { and, eq } from "drizzle-orm";
 
-import { lucia } from '@/auth'
-import db from '@/db'
-import { loginSchema } from '@/validators/auth.schema'
-import { Argon2id } from 'oslo/password'
-
-import { ContextVariables } from '@/types/hono'
+import { lucia } from "@/auth";
+import db from "@/db";
+import { loginSchema } from "@/validators/auth.schema";
+import { Argon2id } from "oslo/password";
 
 export const login = new OpenAPIHono<{
   // Variables: ContextVariables
 }>().openapi(
   createRoute({
-    method: 'post',
-    path: '/auth/login',
-    tags: ['Auth'],
-    summary: 'Login',
-    description: 'Login user with email and password',
+    method: "post",
+    path: "/auth/login",
+    tags: ["Auth"],
+    summary: "Login",
+    description: "Login user with email and password",
     request: {
       body: {
-        description: 'Login Request body',
+        description: "Login Request body",
         content: {
-          'application/json': {
-            schema: loginSchema.openapi('Login', {
+          "application/json": {
+            schema: loginSchema.openapi("Login", {
               example: {
-                email: 'john.doe@example.com',
-                password: 'password123',
+                email: "john.doe@example.com",
+                password: "password123",
               },
             }),
           },
@@ -38,56 +36,54 @@ export const login = new OpenAPIHono<{
     },
     responses: {
       200: {
-        description: 'Success',
+        description: "Success",
       },
       401: {
-        description: 'Unauthorized',
+        description: "Unauthorized",
       },
       500: {
-        description: 'Internal Server Error',
+        description: "Internal Server Error",
       },
     },
   }),
 
   async (c) => {
     try {
-      const { email, password } = c.req.valid('json')
+      const { email, password } = c.req.valid("json");
 
       const existingUser = await db.query.users.findFirst({
         where: and(eq(users.email, email), eq(users.emailVerified, true)),
-      })
+      });
 
       if (!existingUser || !existingUser.hashedPassword) {
-        return c.json({ error: 'Invalid email or password' }, 401)
+        return c.json({ error: "Invalid email or password" }, 401);
       }
 
       const validPassword = await new Argon2id().verify(
         existingUser.hashedPassword,
-        password,
-      )
+        password
+      );
 
       if (!validPassword) {
-        return c.json({ error: 'Invalid email or password' }, 401)
+        return c.json({ error: "Invalid email or password" }, 401);
       }
 
-      const session = await lucia.createSession(existingUser.id, {
-        userApproved: existingUser.userApproved,
-      })
-      const sessionCookie = lucia.createSessionCookie(session.id)
-      const maxAge = Math.min(sessionCookie.attributes.maxAge || 0, 34560000)
+      const session = await lucia.createSession(existingUser.id, {});
+      const sessionCookie = lucia.createSessionCookie(session.id);
+      const maxAge = Math.min(sessionCookie.attributes.maxAge || 0, 34560000);
 
       setCookie(c, sessionCookie.name, sessionCookie.value, {
         ...sessionCookie.attributes,
         maxAge: maxAge,
-        sameSite: 'Strict',
-      })
+        sameSite: "Strict",
+      });
 
-      return c.json({ message: 'Logged in successfully' }, 200)
+      return c.json({ message: "Logged in successfully" }, 200);
     } catch (error) {
       return c.json(
         { error: `An unexpected error occurred during login: ${error}` },
-        500,
-      )
+        500
+      );
     }
-  },
-)
+  }
+);
