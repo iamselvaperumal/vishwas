6f8af9e8b51fc2cebbdf39bba456480466c77338
Jason Richard Kendrick <jasonrichardkendrick@gmail.com>
2024-12-08 18:30:28 +0530

    fixed minor issues

--- src/app/(root)/auth/login/page.tsx
+++ src/app/(root)/auth/login/page.tsx
@@ -1,11 +1,16 @@
-import { getUser } from '@/utils/utils.server'
-import { redirect } from 'next/navigation'
-import LoginForm from './LoginForm'
+import { getUser } from "@/utils/utils.server";
+import { redirect } from "next/navigation";
+import LoginForm from "./LoginForm";
 
 export default async function Login() {
+  const user = await getUser();
+  if (user) {
+    return redirect("/");
+  }
+
   return (
     <>
       <LoginForm />
     </>
-  )
+  );
 }
--- src/app/(root)/auth/register/email-verification/page.tsx
+++ src/app/(root)/auth/register/email-verification/page.tsx
@@ -1,17 +1,17 @@
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 
-import { getUser } from '@/utils/utils.server'
+import { getUser } from "@/utils/utils.server";
 
-import EmailVerificationForm from './EmailVerificationForm'
+import EmailVerificationForm from "./EmailVerificationForm";
 
 export default async function EmailVerification() {
-  const user = await getUser()
+  const user = await getUser();
   if (user?.emailVerified || user) {
-    redirect('/')
+    redirect("/");
   }
   return (
     <>
       <EmailVerificationForm />
     </>
-  )
+  );
 }
